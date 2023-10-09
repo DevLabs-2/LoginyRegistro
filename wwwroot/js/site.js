b9286d4c711a3b2e08643ef94c89aefc89874709
JulianFurland <114943287+JulianFurland@users.noreply.github.com>
2023-10-09 20:58:46 -0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,5 +3,9 @@ function VerifPass() {
   const ochoCaracteres = password.length >= 8;
   const caracterEspecial = /[!@#$&*]/.test(password);
   const mayuscula = /[A-Z]/.test(password);
-  return ochoCaracteres && caracterEspecial && mayuscula;
+  if (ochoCaracteres && caracterEspecial && mayuscula){return true;}
+  else {
+    alert("La contraseña debe tener 8 caracteres o más, uno especial y una mayúscula");
+    return false;
+    }
 }
