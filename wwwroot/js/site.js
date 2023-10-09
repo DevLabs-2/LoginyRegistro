bd87cf459681ffb6dcc570f9df1c24fc5e66b3e6
JulianFurland <114943287+JulianFurland@users.noreply.github.com>
2023-10-09 20:54:51 -0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+function VerifPass() {
+  const password = document.getElementById('contrasenia')
+  const ochoCaracteres = password.length >= 8;
+  const caracterEspecial = /[!@#$&*]/.test(password);
+  const mayuscula = /[A-Z]/.test(password);
+  return ochoCaracteres && caracterEspecial && mayuscula;
+}
